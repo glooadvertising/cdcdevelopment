4884868004d1ed32728f2ca192f409007f66d442
Pablo Sazo <pablo@glooadvertising.com.au>
2023-11-14 16:15:36 +1100

    fade

--- partials/contact.php
+++ partials/contact.php
@@ -1,4 +1,4 @@
-<section id="contact" class="contact-section" data-aos="fade-left">
+<section id="contact" class="contact-section" data-aos="fade-up">
     <div class="container">
         <?php echo do_shortcode( '[fluentform id="1"]' );?>
     </div>
