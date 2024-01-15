4f4fb183223f6c6d607e4bbe84eac9611883c6e2
Pablo Sazo <pablo@glooadvertising.com.au>
2024-01-15 15:56:39 +1100

    Tweaks after Claire's suggestions

--- partials/contact.php
+++ partials/contact.php
@@ -1,5 +1,8 @@
 <section id="contact" class="contact-section" data-aos="fade-up">
     <div class="container">
+        <p><span class="highlighted">Want to discuss your construction requirements?</span></p>
+        <p>To arrange your no-obligation initial consultation, please contact us today on <a href="tel:0362738100">(03) 6273 8100</a></p>
+        <br><br>
         <?php echo do_shortcode( '[fluentform id="1"]' );?>
     </div>
 </section>
